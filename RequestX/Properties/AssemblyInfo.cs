627f53dec3534e0895c881ed819fa212f6751b1b
VisionsXZ <64665287+VisionsXZ@users.noreply.github.com>
2025-11-20 13:37:18 +0400

    Upload Files

---
+++ RequestX/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("RequestX")]
+[assembly: AssemblyDescription("DLL Injector")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DLL Injector")]
+[assembly: AssemblyCopyright("RequestX Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("822E6095-1E0A-40C4-863C-191D8695BEFF")]
+[assembly: AssemblyVersion("5.98.144.2148")]
+[assembly: AssemblyFileVersion("5.98.144.2148")]
